f34588e138c9c93383d8fc548f473ab4146e6f4e
Sunny-Sanjay <92910592+Sunny-Sanjay@users.noreply.github.com>
2021-11-05 23:43:10 -0700

    Add files via upload

---
+++ armatic operations.c
@@ -0,0 +1,15 @@
+// printing all arthematic operations 
+#include<stdio.h>
+void main ()
+{ int a,b,s,p,q,r;
+   a=20;
+   b=20;
+   s=a+b;
+   p=a-b;
+   q=a/b;
+   r=a%b;
+   printf("sum of %d and %d is %d \n" ,a,b,s);
+   printf("sum of %d and %d is %d\n ",a,b,p);
+   printf("difference of %d and %d is %d\n",a,b,q);
+   printf("remainder of %d and %d is %d \n",a,b,r);
+}
